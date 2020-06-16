4989203c0ae476e88227d79773aff55d7e3ae46c
jiangfumei <40990879+jiangfumei@users.noreply.github.com>
2020-06-16 18:31:48 +0800

    解释器模式

---
+++ src/main/java/com/java/study/designPattern/interpreter/Expression.java
@@ -0,0 +1,6 @@
+package com.java.study.designPattern.interpreter;
+
+public interface Expression {
+
+    public int interpret(Context context);
+}
---
+++ src/test/java/com/java/study/designPattern/interpreter/MinusTest.java
@@ -0,0 +1,23 @@
+package com.java.study.designPattern.interpreter;
+
+import com.java.study.BasicTestMvc;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MinusTest extends BasicTestMvc {
+
+    @Test
+    public void minus(){
+        // 计算9+2-8的值
+        int result = new Minus().interpret((new Context(new Plus()
+                .interpret(new Context(9, 2)), 8)));
+        System.out.println(result);
+    }
+
+}
